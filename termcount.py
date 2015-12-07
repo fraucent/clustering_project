2ade14ba7070b3c836ca69193aa99b715b5d40ef
Guillaume Derval <guillaume.derval@student.uclouvain.be>
2015-12-07 16:07:46 +0100

    Improve parsing

--- termcount.py
+++ termcount.py
@@ -1,88 +1,110 @@
 #!/usr/local/bin/python3.5
 # -*-coding:Utf-8 -*
-
+import codecs
 import re
 from html.parser import HTMLParser
 from collections import Counter
 from pickle import Pickler
 
-class CountingParser(HTMLParser) :
-	valid_tags = ['reuters', 'topics', 'title', 'body']
-	def __init__(self, stopwords = []) :
-		self.stopwords = stopwords
-		self.doclist = []
-		self.currenttopics = []
-		self.currentcount = Counter([])
-		self.currenttitle = ''
-		self.currenttag = ''
-		HTMLParser.__init__(self)
-	def handle_starttag(self, tag, attrs) :
-		if tag in CountingParser.valid_tags :
-			self.currenttag = tag
-			print(tag)
-		if tag == 'reuters' :
-			self.currenttopics = []
-			self.currentcount = Counter([])
-	def handle_endtag(self, tag) :
-		if tag in CountingParser.valid_tags :
-			self.currenttag = ''
-		if tag == 'reuters' :
-			if len(self.currenttopics) > 0 :
-				self.doclist.append((self.currenttopics, self.currentcount))
-				#if len(self.currentcount) == 0 :
-				#	print(self.currenttitle)
-			#self.currenttopics = []
-			#self.currentcount = Counter([])
-			#self.currenttitle = ''
-	def handle_data(self, data) :
-		if self.currenttag == 'topics' :
-			self.currenttopics.append(data)
-		elif self.currenttag in ['title', 'body'] :
-			if self.currenttag == 'title' :
-				self.currenttitle = data
-			tokens = re.findall('\w+', data.lower())
-			words = [] # list of words that do not include numbers
-			for token in tokens :
-				if re.search('[a-z]', token) and token not in stopwords :
-					words.append(token)
-			if len(words) == 0 :
-				print('zero words')
-				print(data)
-			self.currentcount.update(words)
-
-with open('data/stopwords') as stwordfile :
-	stopwords = stwordfile.read().split()
-	
-parser = CountingParser(stopwords)
-reuterfnames = [
-	'data/reut2-000.sgm',
-	'data/reut2-001.sgm',
-	'data/reut2-002.sgm',
-	'data/reut2-003.sgm',
-	'data/reut2-004.sgm',
-	'data/reut2-005.sgm',
-	'data/reut2-006.sgm',
-	'data/reut2-007.sgm',
-	'data/reut2-008.sgm',
-	'data/reut2-009.sgm',
-	'data/reut2-010.sgm',
-	'data/reut2-011.sgm',
-	'data/reut2-012.sgm',
-	'data/reut2-013.sgm',
-	'data/reut2-014.sgm',
-	'data/reut2-015.sgm',
-	'data/reut2-016.sgm',
-#	'data/reut2-017.sgm',
-	'data/reut2-018.sgm',
-	'data/reut2-019.sgm',
-	'data/reut2-020.sgm',
-	'data/reut2-021.sgm']
-for fname in reuterfnames :
-	reuterfile = open(fname, 'r')
-	parser.feed(reuterfile.read())
-	reuterfile.close()
-
-with open('reuter_parsed','wb') as reuterp :
-	Pickler(reuterp).dump(parser.doclist)
+import nltk
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
 
 
+#parser = CountingParser(stopwords)
+# reuterfnames = [
+# 	'data/reut2-000.sgm',
+# 	'data/reut2-001.sgm',
+# 	'data/reut2-002.sgm',
+# 	'data/reut2-003.sgm',
+# 	'data/reut2-004.sgm',
+# 	'data/reut2-005.sgm',
+# 	'data/reut2-006.sgm',
+# 	'data/reut2-007.sgm',
+# 	'data/reut2-008.sgm',
+# 	'data/reut2-009.sgm',
+# 	'data/reut2-010.sgm',
+# 	'data/reut2-011.sgm',
+# 	'data/reut2-012.sgm',
+# 	'data/reut2-013.sgm',
+# 	'data/reut2-014.sgm',
+# 	'data/reut2-015.sgm',
+# 	'data/reut2-016.sgm',
+# 	'data/reut2-017.sgm',
+# 	'data/reut2-018.sgm',
+# 	'data/reut2-019.sgm',
+# 	'data/reut2-020.sgm',
+# 	'data/reut2-021.sgm']
+#for fname in reuterfnames :
+#	reuterfile = open(fname, 'r')
+#	parser.feed(reuterfile.read())
+#	reuterfile.close()
+
+#with open('reuter_parsed','wb') as reuterp :
+#	Pickler(reuterp).dump(parser.doclist)
+
+def parse_document(str, stopwords=None):
+    """
+    :param str: the string to tokenize and put in a dict
+    :return: the dictionnary with the number of occurences in each word, the number of different words and the total number of occurences
+    """
+
+    if stopwords is None:
+        stopwords = {}
+
+    data = word_tokenize(str)
+    d = {}
+    for word in data:
+        if word in stopwords:
+            continue
+
+        if word in d:
+            d[word]+=1
+        else:
+            d[word]=1
+
+    s = 0
+    for entry in d.values():
+        s += entry
+
+    return d, len(d), s
+
+def parse_reuters(filename, stopwords):
+    return [parse_document(x.getText(), stopwords) for x in BeautifulSoup(codecs.open(filename, 'r', 'utf-8').read(), "html.parser").find_all(
+        'reuters')]
+
+if __name__ == "__main__":
+    with open('data/stopwords') as stwordfile:
+        stopwords = stwordfile.read().split()
+    reuterfnames = [
+        'data/reut2-000.sgm',
+        'data/reut2-001.sgm',
+        'data/reut2-002.sgm',
+        'data/reut2-003.sgm',
+        'data/reut2-004.sgm',
+        'data/reut2-005.sgm',
+        'data/reut2-006.sgm',
+        'data/reut2-007.sgm',
+        'data/reut2-008.sgm',
+        'data/reut2-009.sgm',
+        'data/reut2-010.sgm',
+        'data/reut2-011.sgm',
+        'data/reut2-012.sgm',
+        'data/reut2-013.sgm',
+        'data/reut2-014.sgm',
+        'data/reut2-015.sgm',
+        'data/reut2-016.sgm',
+    #	'data/reut2-017.sgm',
+        'data/reut2-018.sgm',
+        'data/reut2-019.sgm',
+        'data/reut2-020.sgm',
+        'data/reut2-021.sgm']
+    documents = []
+    for fname in reuterfnames:
+        documents += parse_reuters(fname, stopwords)
+
+    print(documents[0])
+    print(len(documents))
+
+    with open('reuter_parsed', 'wb') as reuterp:
+        Pickler(reuterp).dump(documents)
